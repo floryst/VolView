ecda7904dd1a8855628340cb3605bb6241c30cca
Forrest <forrest.li@kitware.com>
2023-12-04 23:19:39 -0500

    byteDeque tests

--- src/core/streaming/byteDeque.ts
+++ src/core/streaming/byteDeque.ts
@@ -17,6 +17,11 @@ export default class ByteDeque {
     return this.size === 0;
   }
 
+  clear() {
+    this.arrays.length = 0;
+    this._size = 0;
+  }
+
   /**
    * Push a new Uint8Array to the end.
    * @param {Uint8Array} bytes
@@ -27,35 +32,64 @@ export default class ByteDeque {
   }
 
   /**
+   * Push the contents of a ByteDeque to the start.
+   * @param {ByteDeque} bytes
+   */
+  pushStart(bytes: ByteDeque): void;
+
+  /**
    * Push a new Uint8Array to the start.
    * @param {Uint8Array} bytes
    */
-  pushStart(bytes: Uint8Array) {
-    this.arrays.unshift(bytes);
-    this._size += bytes.length;
+  pushStart(bytes: Uint8Array): void;
+
+  pushStart(bytes: Uint8Array | ByteDeque) {
+    if (bytes instanceof ByteDeque) {
+      this.arrays.unshift(...bytes.arrays);
+      this._size += bytes.size;
+      bytes.clear();
+    } else {
+      this.arrays.unshift(bytes);
+      this._size += bytes.length;
+    }
   }
 
   /**
    * Pop <count> bytes off the end.
    * @param {number} count
-   * @param discard do not return the popped bytes
    */
-  popEnd(count: number, discard = false) {
+  popEnd(count: number): Uint8Array {
+    return this.consumeEnd(count, false) as Uint8Array;
+  }
+
+  /**
+   * Erase <count> bytes off the end.
+   *
+   * This is more efficient than ignoring the return value of popEnd
+   * due to not allocating memory for the return result.
+   * @param {number} count
+   */
+  eraseEnd(count: number): void {
+    this.consumeEnd(count, true);
+  }
+
+  private consumeEnd(count: number, discard = false) {
     let processed = 0;
-    const popped = discard ? null : new Uint8Array(count);
-    let writeOffset = 0;
+    const bufferSize = Math.min(count, this.size);
+    const popped = discard ? null : new Uint8Array(bufferSize);
+    let writeOffset = bufferSize;
 
     while (processed < count && this.arrays.length) {
       const bytes = this.arrays.pop()!;
       const remaining = count - processed;
       // chomp bytes[offset:]
-      const offset = remaining >= bytes.length ? 0 : remaining;
+      const offset = remaining >= bytes.length ? 0 : bytes.length - remaining;
       const takeEntireArray = offset === 0;
 
       if (popped) {
         const toSet = takeEntireArray ? bytes : bytes.subarray(offset);
+        writeOffset -= toSet.length;
         popped.set(toSet, writeOffset);
-        writeOffset += toSet.length;
       }
 
       if (!takeEntireArray) {
@@ -66,18 +100,32 @@ export default class ByteDeque {
       processed += bytes.length - offset;
     }
 
-    this._size = Math.max(0, this._size - count);
     return popped ?? undefined;
   }
 
   /**
-   * Pop <count> bytes off the end.
+   * Pop <count> bytes off the start.
    * @param {number} count
-   * @param discard do not return the popped bytes
    */
-  popStart(count: number, discard = false) {
+  popStart(count: number): Uint8Array {
+    return this.consumeStart(count, false) as Uint8Array;
+  }
+
+  /**
+   * Erase <count> bytes off the start.
+   *
+   * This is more efficient than ignoring the return value of popStart
+   * due to not allocating memory for the return result.
+   * @param {number} count
+   */
+  eraseStart(count: number): void {
+    this.consumeStart(count, true);
+  }
+
+  private consumeStart(count: number, discard = false) {
     let processed = 0;
-    const popped = discard ? null : new Uint8Array(count);
+    const bufferSize = Math.min(count, this.size);
+    const popped = discard ? null : new Uint8Array(bufferSize);
     let writeOffset = 0;
 
     while (processed < count && this.arrays.length) {
